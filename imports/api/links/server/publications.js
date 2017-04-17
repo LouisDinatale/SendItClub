9b02d2d75edc7f53faedd7e34c4767b8132c5e3b
Lou <louisdinatale@gmail.com>
2017-04-17 18:05:39 -0400

    updated head and started publication

--- imports/api/links/server/publications.js
+++ imports/api/links/server/publications.js
@@ -2,7 +2,13 @@
 
 import { Meteor } from 'meteor/meteor';
 import { Links } from '../links.js';
+import { UsgsData } from '../usgsData.js'
+
 
 Meteor.publish('links.all', function () {
-  return Links.find();
+	return Links.find();
+});
+
+Meteor.publish('usgs', function () {
+	return UsgsData.find();
 });
